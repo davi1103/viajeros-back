21e83ecdebeda5d53a0c7809c146b7a9ead461c0
davi1103 <ddiazp5@upao.edu.pe>
2023-11-11 16:39:59 -0500

    Primer commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Plataforma_Viajeros"
---
+++ src/test/java/pe/edu/upao/sistemas/Plataforma_Viajeros/PlataformaViajerosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.sistemas.Plataforma_Viajeros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaViajerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
